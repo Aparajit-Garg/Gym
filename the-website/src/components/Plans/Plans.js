775f43067d652ee01dc8bac6763cd0f87f61f83a
Aparajit-Garg <aparajit.garg06@gmail.com>
2023-05-16 23:20:24 +0530

    started the plans section

--- the-website/src/components/Plans/Plans.js
+++ the-website/src/components/Plans/Plans.js
@@ -4,9 +4,16 @@ import classes from "./Plans.module.css";
 const Plans = () => {
 
     return (
-        <div>
-
-        </div>
+        <section>
+            <div className={classes.tag__line}>
+                <span> ready to start</span>
+                <span>your journey</span>
+                <span>with us</span>
+            </div>
+            <div className={classes.__plans}>
+                <h1> Here are the plans</h1>
+            </div>
+        </section>
     );
 }
 
