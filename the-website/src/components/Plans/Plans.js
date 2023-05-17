b5ab579cd7244df81859ba26e162ad9f88fdbd00
Aparajit-Garg <aparajit.garg06@gmail.com>
2023-05-17 10:25:56 +0530

    plans section completed

--- the-website/src/components/Plans/Plans.js
+++ the-website/src/components/Plans/Plans.js
@@ -1,4 +1,6 @@
 import classes from "./Plans.module.css";
+import tickImage from "../../assets/whiteTick.png"; 
+import rightArrow from "../../assets/rightArrow.png";
 
 
 const Plans = () => {
@@ -11,7 +13,91 @@ const Plans = () => {
                 <span>with us</span>
             </div>
             <div className={classes.__plans}>
-                <h1> Here are the plans</h1>
+                <div className={classes.plans__card}>
+                    {/* <img src={} alt="pic" /> */}
+                    <span>basic plan</span>
+                    <span className={classes.plans__price}>
+                        <span>$</span>
+                        <span>25</span>
+                    </span>
+                    
+                    <div className={classes.plan__details}>
+                        <span className={classes.plan__pointers}>
+                            <img src={tickImage} alt="tick pic" />
+                            <span>2 hours of exercises</span>
+                        </span>
+                        <span className={classes.plan__pointers}>
+                            <img src={tickImage} alt="tick pic" />
+                            <span>Free consultation to coaches</span>
+                        </span>
+                        <span className={classes.plan__pointers}>
+                            <img src={tickImage} alt="tick pic" />
+                            <span>Access to the community</span>
+                        </span>
+                        <span>
+                            <span>See more benefits</span>
+                            <img src={rightArrow} alt="" />
+                        </span>
+                        <button>Join Now</button>
+                        
+                    </div>
+                </div>
+                <div className={classes.plans__card}>
+                    {/* <img src={} alt="pic" /> */}
+                    <span>premium plan</span>
+                    <span className={classes.plans__price}>
+                        <span>$</span>
+                        <span>30</span>
+                    </span>
+                    
+                    <div className={classes.plan__details}>
+                        <span className={classes.plan__pointers}>
+                            <img src={tickImage} alt="tick pic" />
+                            <span>5 hours of exercises</span>
+                        </span>
+                        <span className={classes.plan__pointers}>
+                            <img src={tickImage} alt="tick pic" />
+                            <span>Free consultation to coaches</span>
+                        </span>
+                        <span className={classes.plan__pointers}>
+                            <img src={tickImage} alt="tick pic" />
+                            <span>Access to minibar</span>
+                        </span>
+                        <span>
+                            <span>See more benefits</span>
+                            <img src={rightArrow} alt="" />
+                        </span>
+                        <button>Join Now</button>
+                    </div>
+                </div>
+                <div className={classes.plans__card}>
+                    {/* <img src={} alt="pic" /> */}
+                    <span>pro plan</span>
+                    <span className={classes.plans__price}>
+                        <span>$</span>
+                        <span>45</span>
+                    </span>
+                    
+                    <div className={classes.plan__details}>
+                        <span className={classes.plan__pointers}>
+                            <img src={tickImage} alt="tick pic" />
+                            <span>8 hours of exercises</span>
+                        </span>
+                        <span className={classes.plan__pointers}>
+                            <img src={tickImage} alt="tick pic" />
+                            <span>Consultation of Private Coach</span>
+                        </span>
+                        <span className={classes.plan__pointers}>
+                            <img src={tickImage} alt="tick pic" />
+                            <span>Free Fitness Merchandises</span>
+                        </span>
+                        <span>
+                            <span>See more benefits</span>
+                            <img src={rightArrow} alt="" />
+                        </span>
+                        <button>Join Now</button>
+                    </div>
+                </div>
             </div>
         </section>
     );
